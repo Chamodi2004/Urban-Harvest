62a5dab23a11942374f0c779563633ef4ac7dff9
Chamodi2004 <chamodihira@gmail.com>
2026-02-10 14:06:47 +0530

    vite improved

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        about: "pages/about.html",
+        community: "pages/community.html",
+        products: "pages/products.html",
+        subscribe: "pages/subscribe.html",
+      },
+    },
+  },
+});
